68c18cef300d505a08fb92e571ffab38ee1544e4
Pietro Jomini <pietro.jomini@protonmail.ch>
2021-05-07 20:23:26 +0200

    tex clea utility script

---
+++ scripts/tex_clean.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# clean latex compile files
+
+# iterate over desired exts
+for ext in aux fdb_latexmk fls log synctex.gz; do
+
+    # find and delete all file with current ext
+    find . -name *.$ext -delete
+
+done
