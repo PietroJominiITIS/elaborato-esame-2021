04525d9f6121e9388223d7f8185807e08fe8a014
Pietro Jomini <pietro.jomini@protonmail.ch>
2021-05-25 13:19:22 +0200

    cases tree view

--- code/web/index.php
+++ code/web/index.php
@@ -23,13 +23,32 @@ if ($_SESSION['location'] == null) {
             <a href="?page=local" class="<?php  if ($_GET['page'] == 'local') echo 'selected' ?>">Local warehouse</a>
             <a href="?page=global" class="<?php  if ($_GET['page'] == 'global') echo 'selected' ?>">Global catalog</a>
             <a href="?page=transactions" class="<?php  if ($_GET['page'] == 'transactions') echo 'selected' ?>">Transactions</a>
+            <a href="?page=queries" class="<?php  if ($_GET['page'] == 'queries') echo 'selected' ?>">Queries</a>
         </div>
 
     </div>
 
     <div class="page">
         <?php  if ($_GET['page'] == 'local') { ?>
-            local page
+
+            <div class="tree">
+                <div class="case">
+                    <div class="title">case</div>
+                    <div class="box">
+                        <div class="title">box1</div>
+                        <a href="?page=local&item=a" class="<?php  if ($_GET['item'] == 'a') echo 'selected' ?>">item a</a>
+                        <a href="?page=local&item=b" class="<?php  if ($_GET['item'] == 'b') echo 'selected' ?>">item b</a>
+                        <a href="?page=local&item=c" class="<?php  if ($_GET['item'] == 'c') echo 'selected' ?>">item c</a>
+                    </div>
+                    <div class="box">
+                        <div class="title">box1</div>
+                        <a href="?page=local&item=a1" class="<?php  if ($_GET['item'] == 'a1') echo 'selected' ?>">item a</a>
+                        <a href="?page=local&item=b1" class="<?php  if ($_GET['item'] == 'b1') echo 'selected' ?>">item b</a>
+                        <a href="?page=local&item=c1" class="<?php  if ($_GET['item'] == 'c1') echo 'selected' ?>">item c</a>
+                    </div>
+                </div>
+            </div>
+
         <?php } ?>
 
         <?php  if ($_GET['page'] == 'global') { ?>
@@ -38,6 +57,10 @@ if ($_SESSION['location'] == null) {
 
         <?php  if ($_GET['page'] == 'transactions') { ?>
             transactions page
+        <?php } ?>
+
+        <?php  if ($_GET['page'] == 'queries') { ?>
+            queries page
         <?php } ?>
     </div>
 
