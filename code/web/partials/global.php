65d314e9145f4bc43ec6d5f3ede02bc8f8585508
Pietro Jomini <pietro.jomini@protonmail.ch>
2021-05-28 23:30:39 +0200

    items tree view ui

--- code/web/partials/global.php
+++ code/web/partials/global.php
@@ -45,85 +45,90 @@
 <?php } ?>
 
 <?php if($_GET['box'] != null) { ?>
+<div class="middlebar">
+    <div class="title">Items</div>
+    <div class="container">
 
-    <!-- document -->
-    <div class="middlebar">
-        <div class="title">Document</div>
+        <!-- document -->
+        <div class="middlebar">
+            <div class="title">Document</div>
 
-        <?php
-            $cmd = $connection->prepare("
-                SELECT DocumentID, Type FROM Document
-                JOIN Box ON Document.Box = Box.BoxId
-                WHERE Location <> '" . $_SESSION['location'] . "' and Box = '" . $_GET['box'] . "'");
-            $cmd->execute();
+            <?php
+                $cmd = $connection->prepare("
+                    SELECT DocumentID, Type FROM Document
+                    JOIN Box ON Document.Box = Box.BoxId
+                    WHERE Location <> '" . $_SESSION['location'] . "' and Box = '" . $_GET['box'] . "'");
+                $cmd->execute();
 
-            foreach ($cmd->fetchAll(PDO::FETCH_ASSOC) as $row) {
-                $params = "page=item&src=global&type=document&case=" . $_GET['case'] . "&box=" . $_GET['box'] . "&id=" . $row['DocumentID'];
-                echo "<a href='../pages/index.php?$params'>" . $row['DocumentID'] . ' - ' . $row['Type'] . "</a>";
-            }
-        ?>
+                foreach ($cmd->fetchAll(PDO::FETCH_ASSOC) as $row) {
+                    $params = "page=item&src=global&type=document&case=" . $_GET['case'] . "&box=" . $_GET['box'] . "&id=" . $row['DocumentID'];
+                    echo "<a href='../pages/index.php?$params'>" . $row['DocumentID'] . ' - ' . $row['Type'] . "</a>";
+                }
+            ?>
 
-        <a href="../pages/index.php?page=create&type=document&case=<?php echo $_GET['case'] ?>&box=<?php echo $_GET['box'] ?>" class="add">+</a>
+            <a href="../pages/index.php?page=create&type=document&case=<?php echo $_GET['case'] ?>&box=<?php echo $_GET['box'] ?>" class="add">+</a>
+        </div>
+
+        <!-- evidence -->
+        <div class="middlebar">
+            <div class="title">Evidence</div>
+
+            <?php
+                $cmd = $connection->prepare("
+                    SELECT EvidenceID, Label FROM Evidence
+                    JOIN Box ON Evidence.Box = Box.BoxId
+                    WHERE Location <> '" . $_SESSION['location'] . "' and Box = '" . $_GET['box'] . "'");
+                $cmd->execute();
+
+                foreach ($cmd->fetchAll(PDO::FETCH_ASSOC) as $row) {
+                    $params = "page=item&src=global&type=evidence&case=" . $_GET['case'] . "&box=" . $_GET['box'] . "&id=" . $row['EvidenceID'];
+                    echo "<a href='../pages/index.php?$params'>" . $row['EvidenceID'] . ' - ' . $row['Label'] . "</a>";
+                }
+            ?>
+
+            <a href="../pages/index.php?page=create&type=evidence&case=<?php echo $_GET['case'] ?>&box=<?php echo $_GET['box'] ?>" class="add">+</a>
+        </div>
+
+        <!-- report -->
+        <div class="middlebar">
+            <div class="title">Report</div>
+
+            <?php
+                $cmd = $connection->prepare("
+                    SELECT ReportID, Place FROM Report
+                    JOIN Box ON Report.Box = Box.BoxId
+                    WHERE Location <> '" . $_SESSION['location'] . "' and Box = '" . $_GET['box'] . "'");
+                $cmd->execute();
+
+                foreach ($cmd->fetchAll(PDO::FETCH_ASSOC) as $row) {
+                    $params = "page=item&src=global&type=report&case=" . $_GET['case'] . "&box=" . $_GET['box'] . "&id=" . $row['ReportID'];
+                    echo "<a href='../pages/index.php?$params'>" . $row['ReportID'] . ' - ' . $row['Place'] . "</a>";
+                }
+            ?>
+
+            <a href="../pages/index.php?page=create&type=report&case=<?php echo $_GET['case'] ?>&box=<?php echo $_GET['box'] ?>" class="add">+</a>
+        </div>
+
+        <!-- paper -->
+        <div class="middlebar">
+            <div class="title">Paper</div>
+
+            <?php
+                $cmd = $connection->prepare("
+                    SELECT PaperID, PersonCF FROM Paper
+                    JOIN Box ON Paper.Box = Box.BoxId
+                    WHERE Location <> '" . $_SESSION['location'] . "' and Box = '" . $_GET['box'] . "'");
+                $cmd->execute();
+
+                foreach ($cmd->fetchAll(PDO::FETCH_ASSOC) as $row) {
+                    $params = "page=item&src=global&type=paper&case=" . $_GET['case'] . "&box=" . $_GET['box'] . "&id=" . $row['PaperID'];
+                    echo "<a href='../pages/index.php?$params'>" . $row['PaperID'] . ' - ' . $row['PersonCF'] . "</a>";
+                }
+            ?>
+
+            <a href="../pages/index.php?page=create&type=paper&case=<?php echo $_GET['case'] ?>&box=<?php echo $_GET['box'] ?>" class="add">+</a>
+        </div>
     </div>
-
-    <!-- evidence -->
-    <div class="middlebar">
-        <div class="title">Evidence</div>
-
-        <?php
-            $cmd = $connection->prepare("
-                SELECT EvidenceID, Label FROM Evidence
-                JOIN Box ON Evidence.Box = Box.BoxId
-                WHERE Location <> '" . $_SESSION['location'] . "' and Box = '" . $_GET['box'] . "'");
-            $cmd->execute();
-
-            foreach ($cmd->fetchAll(PDO::FETCH_ASSOC) as $row) {
-                $params = "page=item&src=global&type=evidence&case=" . $_GET['case'] . "&box=" . $_GET['box'] . "&id=" . $row['EvidenceID'];
-                echo "<a href='../pages/index.php?$params'>" . $row['EvidenceID'] . ' - ' . $row['Label'] . "</a>";
-            }
-        ?>
-
-        <a href="../pages/index.php?page=create&type=evidence&case=<?php echo $_GET['case'] ?>&box=<?php echo $_GET['box'] ?>" class="add">+</a>
-    </div>
-
-    <!-- report -->
-    <div class="middlebar">
-        <div class="title">Report</div>
-
-        <?php
-            $cmd = $connection->prepare("
-                SELECT ReportID, Place FROM Report
-                JOIN Box ON Report.Box = Box.BoxId
-                WHERE Location <> '" . $_SESSION['location'] . "' and Box = '" . $_GET['box'] . "'");
-            $cmd->execute();
-
-            foreach ($cmd->fetchAll(PDO::FETCH_ASSOC) as $row) {
-                $params = "page=item&src=global&type=report&case=" . $_GET['case'] . "&box=" . $_GET['box'] . "&id=" . $row['ReportID'];
-                echo "<a href='../pages/index.php?$params'>" . $row['ReportID'] . ' - ' . $row['Place'] . "</a>";
-            }
-        ?>
-
-        <a href="../pages/index.php?page=create&type=report&case=<?php echo $_GET['case'] ?>&box=<?php echo $_GET['box'] ?>" class="add">+</a>
-    </div>
-
-    <!-- paper -->
-    <div class="middlebar">
-        <div class="title">Paper</div>
-
-        <?php
-            $cmd = $connection->prepare("
-                SELECT PaperID, PersonCF FROM Paper
-                JOIN Box ON Paper.Box = Box.BoxId
-                WHERE Location <> '" . $_SESSION['location'] . "' and Box = '" . $_GET['box'] . "'");
-            $cmd->execute();
-
-            foreach ($cmd->fetchAll(PDO::FETCH_ASSOC) as $row) {
-                $params = "page=item&src=global&type=paper&case=" . $_GET['case'] . "&box=" . $_GET['box'] . "&id=" . $row['PaperID'];
-                echo "<a href='../pages/index.php?$params'>" . $row['PaperID'] . ' - ' . $row['PersonCF'] . "</a>";
-            }
-        ?>
-
-        <a href="../pages/index.php?page=create&type=paper&case=<?php echo $_GET['case'] ?>&box=<?php echo $_GET['box'] ?>" class="add">+</a>
-    </div>
+</div>
 
 <?php } ?>
