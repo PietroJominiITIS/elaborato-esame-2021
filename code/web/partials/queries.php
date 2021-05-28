d32a7949bd79378c80fc81d8e117b9fa4ef174ec
Pietro Jomini <pietro.jomini@protonmail.ch>
2021-05-29 00:07:40 +0200

    queries page ui

---
+++ code/web/partials/queries.php
@@ -0,0 +1,64 @@
+<link rel="stylesheet" href="../css/queries.css">
+
+<div class="queries">
+    <div class="query">
+        <div class="title">Select all</div>
+        <code>SELECT * FROM Document JOIN Box ON Document.Box = Box.BoxID</code>
+        <table>
+            <tr>
+                <th>Field1</th>
+                <th>Field2</th>
+                <th>A</th>
+                <th>HEY HEY</th>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>hey</td>
+                <td>null</td>
+                <td>hoy hoy hoy</td>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>hey</td>
+                <td>null</td>
+                <td>hoy hoy hoy</td>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>hey</td>
+                <td>null</td>
+                <td>hoy hoy hoy</td>
+            </tr>
+        </table>
+    </div>
+    <div class="query">
+        <div class="title">Select all</div>
+        <code>SELECT * FROM Document JOIN Box ON Document.Box = Box.BoxID</code>
+        <table>
+            <tr>
+                <th>Field1</th>
+                <th>Field2</th>
+                <th>A</th>
+                <th>HEY HEY</th>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>hey</td>
+                <td>null</td>
+                <td>hoy hoy hoy</td>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>hey</td>
+                <td>null</td>
+                <td>hoy hoy hoy</td>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>hey</td>
+                <td>null</td>
+                <td>hoy hoy hoy</td>
+            </tr>
+        </table>
+    </div>
+</div>
