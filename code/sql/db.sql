56546ad3b5502f6b941cb15582718e3e59dba19c
Pietro Jomini <pietro.jomini@protonmail.ch>
2021-05-30 11:25:45 +0200

    normalized apostrophes

--- code/sql/db.sql
+++ code/sql/db.sql
@@ -1,16 +1,16 @@
 -- TABLE BOX
 
-CREATE TABLE `Box` (
-  `BoxId` int(11) NOT NULL AUTO_INCREMENT,
-  `CaseT` int(11) NOT NULL,
-  `Location` varchar(2) NOT NULL,
-  `ArchivingDate` date NOT NULL,
-  `DeletionDate` date DEFAULT NULL,
+CREATE TABLE 'Box' (
+  'BoxId' int(11) NOT NULL AUTO_INCREMENT,
+  'CaseT' int(11) NOT NULL,
+  'Location' varchar(2) NOT NULL,
+  'ArchivingDate' date NOT NULL,
+  'DeletionDate' date DEFAULT NULL,
   'DeletionReport' TEXT DEFAULT NULL,
 
   PRIMARY KEY ('BoxId'),
-  FOREIGN KEY (`CaseT`) REFERENCES `CaseT` (`CaseId`),
-  FOREIGN KEY (`Location`) REFERENCES `Court` (`Location`),
+  FOREIGN KEY ('CaseT') REFERENCES 'CaseT' ('CaseId'),
+  FOREIGN KEY ('Location') REFERENCES 'Court' ('Location'),
 
   CHECK ('DeletionDate' IS NULL OR YEAR('DeletionDate') - YEAR('ArchivingDate') >= 50),
   CHECK ('DeletionDate' IS NULL OR 'DeletionReport' IS NOT NULL)
@@ -19,28 +19,28 @@ CREATE TABLE `Box` (
 
 --  TABLE CaseOutcome
 
-CREATE TABLE `CaseOutcome` (
-  `Name` varchar(20) NOT NULL,
-  `Description` text NOT NULL,
+CREATE TABLE 'CaseOutcome' (
+  'Name' varchar(20) NOT NULL,
+  'Description' text NOT NULL,
 
-  PRIMARY KEY (`Name`)
+  PRIMARY KEY ('Name')
 );
 
-INSERT INTO `CaseOutcome` (`Name`, `Description`) VALUES
+INSERT INTO 'CaseOutcome' ('Name', 'Description') VALUES
   ('Condemned', ''),
   ('Acquitted', '');
 
 
 -- TABLE CaseStatus
 
-CREATE TABLE `CaseStatus` (
-  `Name` varchar(20) NOT NULL,
-  `Description` text NOT NULL,
+CREATE TABLE 'CaseStatus' (
+  'Name' varchar(20) NOT NULL,
+  'Description' text NOT NULL,
 
-  PRIMARY KEY (`Name`)
+  PRIMARY KEY ('Name')
 );
 
-INSERT INTO `CaseStatus` (`Name`, `Description`) VALUES
+INSERT INTO 'CaseStatus' ('Name', 'Description') VALUES
   ('Preliminary', ''),
   ('First', ''),
   ('Second', ''),
@@ -50,32 +50,32 @@ INSERT INTO `CaseStatus` (`Name`, `Description`) VALUES
 
 -- TABLE CaseT
 
-CREATE TABLE `CaseT` (
-  `CaseId` int(11) NOT NULL AUTO_INCREMENT,
-  `Court` varchar(2) NOT NULL,
-  `Status` varchar(20) DEFAULT NULL,
-  `Outcome` varchar(20) DEFAULT NULL,
-  `Crime` text NOT NULL,
-  `Year` int(4) NOT NULL,
+CREATE TABLE 'CaseT' (
+  'CaseId' int(11) NOT NULL AUTO_INCREMENT,
+  'Court' varchar(2) NOT NULL,
+  'Status' varchar(20) DEFAULT NULL,
+  'Outcome' varchar(20) DEFAULT NULL,
+  'Crime' text NOT NULL,
+  'Year' int(4) NOT NULL,
 
-  PRIMARY KEY (`CaseId`),
-  FOREIGN KEY (`Status`) REFERENCES `CaseStatus` (`Name`) ON UPDATE CASCADE,
-  FOREIGN KEY (`Outcome`) REFERENCES `CaseOutcome` (`Name`) ON UPDATE CASCADE,
-  FOREIGN KEY (`Court`) REFERENCES `Court` (`Location`)
+  PRIMARY KEY ('CaseId'),
+  FOREIGN KEY ('Status') REFERENCES 'CaseStatus' ('Name') ON UPDATE CASCADE,
+  FOREIGN KEY ('Outcome') REFERENCES 'CaseOutcome' ('Name') ON UPDATE CASCADE,
+  FOREIGN KEY ('Court') REFERENCES 'Court' ('Location')
 );
 
 
 -- TABLE Court
 
-CREATE TABLE `Court` (
-  `Location` varchar(2) NOT NULL,
-  `Name` text NOT NULL,
-  `Password` varchar(20) NOT NULL,
+CREATE TABLE 'Court' (
+  'Location' varchar(2) NOT NULL,
+  'Name' text NOT NULL,
+  'Password' varchar(20) NOT NULL,
 
-  PRIMARY KEY (`Location`)
+  PRIMARY KEY ('Location')
 );
 
-INSERT INTO `Court` (`Name`, 'Password') VALUES
+INSERT INTO 'Court' ('Name', 'Password') VALUES
   ('AL', 'AL'),
   ('AT', 'AT'),
   ('BI', 'BI'),
@@ -88,28 +88,28 @@ INSERT INTO `Court` (`Name`, 'Password') VALUES
 
 -- TABLE Document
 
-CREATE TABLE `Document` (
-  `DocumentId` int(11) NOT NULL AUTO_INCREMENT,
-  `Box` int(11) NOT NULL,
-  `Type` varchar(20) NOT NULL,
-  `Description` text NOT NULL,
-  `AcquisitionDate` date NOT NULL,
+CREATE TABLE 'Document' (
+  'DocumentId' int(11) NOT NULL AUTO_INCREMENT,
+  'Box' int(11) NOT NULL,
+  'Type' varchar(20) NOT NULL,
+  'Description' text NOT NULL,
+  'AcquisitionDate' date NOT NULL,
 
-  PRIMARY KEY (`DocumentId`),
-  FOREIGN KEY (`Box`) REFERENCES `Box` (`BoxId`),
-  FOREIGN KEY (`Type`) REFERENCES `DocumentType` (`Name`) ON UPDATE CASCADE
+  PRIMARY KEY ('DocumentId'),
+  FOREIGN KEY ('Box') REFERENCES 'Box' ('BoxId'),
+  FOREIGN KEY ('Type') REFERENCES 'DocumentType' ('Name') ON UPDATE CASCADE
 );
 
 
 -- TABLE DocumentType
 
-CREATE TABLE `DocumentType` (
-  `Name` varchar(20) NOT NULL,
+CREATE TABLE 'DocumentType' (
+  'Name' varchar(20) NOT NULL,
 
-  PRIMARY KEY (`Name`)
+  PRIMARY KEY ('Name')
 );
 
-INSERT INTO `DocumentType` (`Name`) VALUES
+INSERT INTO 'DocumentType' ('Name') VALUES
   ('Letter'),
   ('Photo'),
   ('Receipt');
@@ -117,75 +117,75 @@ INSERT INTO `DocumentType` (`Name`) VALUES
 
 -- TABLE Evidence
 
-CREATE TABLE `Evidence` (
-  `EvidenceId` int(11) NOT NULL AUTO_INCREMENT,
-  `Box` int(11) NOT NULL,
-  `Description` text NOT NULL,
-  `AcquisitionDate` date NOT NULL,
-  `Label` text NOT NULL,
+CREATE TABLE 'Evidence' (
+  'EvidenceId' int(11) NOT NULL AUTO_INCREMENT,
+  'Box' int(11) NOT NULL,
+  'Description' text NOT NULL,
+  'AcquisitionDate' date NOT NULL,
+  'Label' text NOT NULL,
 
-  PRIMARY KEY (`EvidenceId`),
-  FOREIGN KEY (`Box`) REFERENCES `Box` (`BoxId`)
+  PRIMARY KEY ('EvidenceId'),
+  FOREIGN KEY ('Box') REFERENCES 'Box' ('BoxId')
 );
 
 
 -- TABLE Paper
 
-CREATE TABLE `Paper` (
-  `PaperId` int(11) NOT NULL AUTO_INCREMENT,
-  `Box` int(11) NOT NULL,
-  `Place` text NOT NULL,
-  `PersonCF` varchar(16) NOT NULL,
-  `RegistrationDate` date NOT NULL,
+CREATE TABLE 'Paper' (
+  'PaperId' int(11) NOT NULL AUTO_INCREMENT,
+  'Box' int(11) NOT NULL,
+  'Place' text NOT NULL,
+  'PersonCF' varchar(16) NOT NULL,
+  'RegistrationDate' date NOT NULL,
 
-  PRIMARY KEY (`PaperId`)
-  FOREIGN KEY (`Box`) REFERENCES `Box` (`BoxId`)
+  PRIMARY KEY ('PaperId')
+  FOREIGN KEY ('Box') REFERENCES 'Box' ('BoxId')
 );
 
 
 -- TABLE Report
 
-CREATE TABLE `Report` (
-  `ReportId` int(11) NOT NULL AUTO_INCREMENT,
-  `Box` int(11) NOT NULL,
-  `Place` text NOT NULL,
-  `Date` date NOT NULL,
+CREATE TABLE 'Report' (
+  'ReportId' int(11) NOT NULL AUTO_INCREMENT,
+  'Box' int(11) NOT NULL,
+  'Place' text NOT NULL,
+  'Date' date NOT NULL,
 
-  PRIMARY KEY (`ReportId`)
-  FOREIGN KEY (`Box`) REFERENCES `Box` (`BoxId`)
+  PRIMARY KEY ('ReportId')
+  FOREIGN KEY ('Box') REFERENCES 'Box' ('BoxId')
 );
 
 
 -- TABLE Transition
 
-CREATE TABLE `Transition` (
-  `TransitionId` int(11) NOT NULL AUTO_INCREMENT,
-  `Box` int(11) NOT NULL,
-  `FromL` varchar(2) NOT NULL,
-  `ToL` varchar(2) NOT NULL,
-  `Status` varchar(20) NOT NULL,
-  `DepartureTime` date DEFAULT NULL,
-  `ArrivalTime` date DEFAULT NULL,
-  `AgentCF` varchar(16) DEFAULT NULL,
+CREATE TABLE 'Transition' (
+  'TransitionId' int(11) NOT NULL AUTO_INCREMENT,
+  'Box' int(11) NOT NULL,
+  'FromL' varchar(2) NOT NULL,
+  'ToL' varchar(2) NOT NULL,
+  'Status' varchar(20) NOT NULL,
+  'DepartureTime' date DEFAULT NULL,
+  'ArrivalTime' date DEFAULT NULL,
+  'AgentCF' varchar(16) DEFAULT NULL,
 
-  PRIMARY KEY (`TransitionId`),
-  FOREIGN KEY (`Box`) REFERENCES `Box` (`BoxId`),
-  FOREIGN KEY (`FromL`) REFERENCES `Court` (`Location`),
-  FOREIGN KEY (`ToL`) REFERENCES `Court` (`Location`),
-  FOREIGN KEY (`Status`) REFERENCES `TransitionStatus` (`Name`) ON UPDATE CASCADE
+  PRIMARY KEY ('TransitionId'),
+  FOREIGN KEY ('Box') REFERENCES 'Box' ('BoxId'),
+  FOREIGN KEY ('FromL') REFERENCES 'Court' ('Location'),
+  FOREIGN KEY ('ToL') REFERENCES 'Court' ('Location'),
+  FOREIGN KEY ('Status') REFERENCES 'TransitionStatus' ('Name') ON UPDATE CASCADE
 );
 
 
 -- TABLE TransitionStatus
 
-CREATE TABLE `TransitionStatus` (
-  `Name` varchar(20) NOT NULL,
-  `Description` text NOT NULL,
+CREATE TABLE 'TransitionStatus' (
+  'Name' varchar(20) NOT NULL,
+  'Description' text NOT NULL,
 
-  ADD PRIMARY KEY (`Name`)
+  ADD PRIMARY KEY ('Name')
 );
 
-INSERT INTO `TransitionStatus` (`Name`, `Description`) VALUES
+INSERT INTO 'TransitionStatus' ('Name', 'Description') VALUES
   ('Accepted', 'The destintion court requested the transition'),
   ('Completed', 'The origin court accepted the transition'),
   ('Requested', 'The transition is taking place'),
