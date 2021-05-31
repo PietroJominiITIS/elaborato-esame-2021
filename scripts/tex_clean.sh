cb66d582b2403991d8ee0114352cf18ad6baa997
Pietro Jomini <pietro.jomini@protonmail.ch>
2021-05-31 10:14:11 +0200

    tex clean .out

--- scripts/tex_clean.sh
+++ scripts/tex_clean.sh
@@ -2,7 +2,7 @@
 # clean latex compile files
 
 # iterate over desired exts
-for ext in aux fdb_latexmk fls log synctex.gz toc; do
+for ext in aux fdb_latexmk fls log synctex.gz out toc; do
 
     # find and delete all file with current ext
     find . -name *.$ext -delete
